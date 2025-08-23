9c3cf3e58a54ae3a700c019be150e8f9afd60e67
Kaspar Bergert <kasparbergert6@gmail.com>
2025-08-23 20:08:35 +0300

    added cors

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ import "./env.js"; //importing the .env file.
 import express from "express";
 import db from "./database/init.js";
 import MakeJWTRoute from "./JWT/route.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true //able to recieve cookies
+}));
+
 
 app.use(express.json());
 app.post("/", (req, res) => {
